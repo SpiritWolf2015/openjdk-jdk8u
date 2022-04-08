8656b282adcc914dd9d5f810055243116e975b08
SpiritWolf <spirit_wof2016@qq.com>
2022-04-09 00:22:46 +0800

    [GZC]ReentrantReadWriteLock的tryAcquire方法

--- jdk/src/share/classes/java/util/concurrent/locks/ReentrantReadWriteLock.java
+++ jdk/src/share/classes/java/util/concurrent/locks/ReentrantReadWriteLock.java
@@ -389,11 +389,20 @@ public class ReentrantReadWriteLock
              *    queue policy allows it. If so, update state
              *    and set owner.
              */
+            /*
+            1.写锁是一个支持重进入的排它锁。如果当前线程已经获取了写锁，则增加写状态。如果当前线程在获取写锁时，
+            读锁已经被获取（读状态不为0）或者该线程不是已经获取写锁的线程，则当前线程进入等待状态。
+            2.该方法除了重入条件（当前线程为获取了写锁的线程）之外，增加了一个读锁是否存在的判断。如果存在读锁，
+            则写锁不能被获取，原因在于：读写锁要确保写锁的操作对读锁可见，如果允许读锁在已被获取的情况下对写锁的获取，
+            那么正在运行的其他读线程就无法感知到当前写线程的操作。因此，只有等待其他读线程都释放了读锁，
+            写锁才能被当前线程获取，而写锁一旦被获取，则其他读写线程的后续访问均被阻塞。
+            */
             Thread current = Thread.currentThread();
             int c = getState();
             int w = exclusiveCount(c);
             if (c != 0) {
                 // (Note: if c != 0 and w == 0 then shared count != 0)
+                // 存在读锁或者当前获取线程不是已经获取写锁的线程
                 if (w == 0 || current != getExclusiveOwnerThread())
                     return false;
                 if (w + exclusiveCount(acquires) > MAX_COUNT)
@@ -403,8 +412,9 @@ public class ReentrantReadWriteLock
                 return true;
             }
             if (writerShouldBlock() ||
-                !compareAndSetState(c, c + acquires))
+                    !compareAndSetState(c, c + acquires)) {
                 return false;
+            }
             setExclusiveOwnerThread(current);
             return true;
         }
